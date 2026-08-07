ee753b386fdbffb27d1c48130253d3f8ea2c6212
Daniel Li <danielli8008@gmail.com>
2026-08-07 17:38:37 -0400

    backend testing

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello, World!")
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+	// Return a greeting that embeds the name in a message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
 }
